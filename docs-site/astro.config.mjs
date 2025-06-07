45a77368783f05c9b8588126aff11d8c54ca2b64
Robert Vitonsky <robert@vitonsky.net>
2025-06-08 00:32:13 +0200

    chore: add base path

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://vitonsky.github.io/nano-queries/',
+	base: '/nano-queries',
 	integrations: [
 		starlight({
 			title: 'NanoQueries',
