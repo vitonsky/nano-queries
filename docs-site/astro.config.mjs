0a8e03f85cbaa556d16311f7cf5afca75ec6fba5
Robert Vitonsky <robert@vitonsky.net>
2025-06-08 00:08:31 +0200

    chore: add site with docs

---
+++ docs-site/astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'NanoQueries',
+			logo: {
+				src: './logo.svg'
+			},
+			pagefind: false,
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/vitonsky/nano-queries' },
+				{ icon: 'npm', label: 'NPM', href: 'https://www.npmjs.com/package/nano-queries' },
+			],
+			sidebar: [
+			],
+		}),
+	],
+});
