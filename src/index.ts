a0e217d170eedc384a06b10d607667303c2a96eb
Robert Vitonsky <robert@vitonsky.net>
2026-01-23 23:26:45 +0100

    fix: export builders

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export * from './core/RawSegment';
 export * from './core/PreparedValue';
 export * from './core/Query';
 
+// Builders
+export * from './QueryBuilder';
+export * from './TemplateStringQueryBuilder';
+
 // Compilers
 export * from './compilers/SQLCompiler';
 
@@ -11,3 +15,6 @@ export * from './utils/segments';
 
 // SQL
 export * from './sql/ConfigurableSQLBuilder';
+
+// Types
+export * from './types';
